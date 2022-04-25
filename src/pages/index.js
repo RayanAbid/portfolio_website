ec8e7263c366624a0a334d8cd247b8e764e55c7c
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2022-04-25 10:17:22 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,16 @@ import Technologies from "../components/Technologies/Technologies";
 import Timeline from "../components/TimeLine/TimeLine";
 import { Layout } from "../layout/Layout";
 import { Section } from "../styles/GlobalComponents";
+import Head from 'next/head'
 
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <title>Rayan Abid</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="google-site-verification" content="NlCmj9T2EobwjHW2Gdvn8HCmRssQsPMjRNxKPX30G5I" />
+      </Head>
       <Section grid>
         <Hero />
         <BgAnimation />
